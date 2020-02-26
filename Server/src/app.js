c3e80ceae7571d1c07b169148d454d50ab0dacfd
Michael DiGiorgio <miked42591@gmail.com>
2020-02-26 14:32:01 -0500

    heroku

--- Server/src/app.js
+++ Server/src/app.js
@@ -51,7 +51,7 @@ app.use('/api', apiRouter);
 
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../Client/build')));
-  app.get('/', function(req, res) {
+  app.get('*', function(req, res) {
     res.sendFile('index.html', {root: path.join(__dirname, '../../Client/build/')})
   });
 }
